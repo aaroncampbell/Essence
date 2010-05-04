007bd3b6b7e71fbcf7ed3875f0484dd4d070b45e
aaroncampbell <aaroncampbell@cdb4dc37-54d1-4dfa-a9af-8c49da855a76>
2010-05-04 21:56:11 +0000

    Remove a comment and add readme.txt

---
+++ readme.txt
@@ -0,0 +1,4 @@
+@todo Fix this whole section of notes
+
+Supports Yoast BreadCrumbs
+Supports Twitter Widget Pro
--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,6 @@
 <?php
 /**
  * The Sidebar containing the primary and secondary widget areas
- *
- * @package WordPress
- * @subpackage Twenty Ten
- * @since 3.0.0
  */
 ?>
 
