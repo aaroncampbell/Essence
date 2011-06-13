67b76a5f0b519d3df0b63712c8adc38a80ac172c
aaron@xavisys.com <aaron@xavisys.com@cdb4dc37-54d1-4dfa-a9af-8c49da855a76>
2011-06-13 02:27:03 +0000

    Code formatting, filter naming fixes, and style name fixes

--- lib/admin/menu.php
+++ lib/admin/menu.php
@@ -1,13 +1,13 @@
 <?php
-add_action('admin_menu', 'essence_admin_menu_setup');
+add_action( 'admin_menu', 'essence_admin_menu_setup' );
 function essence_admin_menu_setup() {
 	add_theme_page( __( 'Settings', 'essence' ), __( 'Settings', 'essence' ), 'manage_options', 'essence-settings', 'essence_theme_admin_settings' );
 }
 
 add_action( 'admin_print_styles-appearance_page_essence-settings', 'essence_admin_page_styles' );
 function essence_admin_page_styles() {
-	wp_enqueue_style('dashboard');
-	wp_enqueue_style('xavisys-options-css', ESSENCE_ADMIN_URL . '/essence-admin.css');
+	wp_enqueue_style( 'dashboard' );
+	wp_enqueue_style( 'essence-options-css', ESSENCE_ADMIN_URL . '/essence-admin.css' );
 }
 
 add_action( 'admin_print_scripts-appearance_page_essence-settings', 'essence_admin_page_scripts' );
@@ -16,15 +16,15 @@ function essence_admin_page_scripts() {
 	wp_enqueue_script( 'dashboard' );
 }
 
-function essence_screen_icon_link($name = 'essence') {
-	$link = '<a href="http://xavisys.com">';
+function essence_screen_icon_link( $name = 'essence' ) {
+	$link = '<a href="http://gotthemes.com">';
 	if ( function_exists( 'get_screen_icon' ) ) {
 		$link .= get_screen_icon( $name );
 	} else {
 		ob_start();
-		screen_icon($name);
+		screen_icon( $name );
 		$link .= ob_get_clean();
 	}
 	$link .= '</a>';
-	echo apply_filters('xpf-screenIconLink', $link, $name );
+	echo apply_filters( 'essence-screen-icon-link', $link, $name );
 }
