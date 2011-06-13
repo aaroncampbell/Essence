1c412312443714841f79c9e61af1897251cedbc0
aaron@xavisys.com <aaron@xavisys.com@cdb4dc37-54d1-4dfa-a9af-8c49da855a76>
2011-06-13 02:53:35 +0000

    Superfish menus should work with page lists

--- js/superfish.args.js
+++ js/superfish.args.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 
-	$('ul.menu, ul.submenu').superfish({
+	$('ul.menu, ul.submenu, div.menu ul, div.submenu ul').superfish({
 		delay:       100,								// .5 second delay on mouseout
 		animation:   {opacity:'show',height:'show'},	// fade-in and slide-down animation
 		dropShadows: false								// disable drop shadows
