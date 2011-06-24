1af234c89cb0dbaab4d892693634c6168bd09cc5
aaron@xavisys.com <aaron@xavisys.com@cdb4dc37-54d1-4dfa-a9af-8c49da855a76>
2011-06-24 22:52:26 +0000

    Add ie6, ie7, or ie8 class to html tag and use the new essence_title()

--- header.php
+++ header.php
@@ -12,28 +12,22 @@ if ( !empty( $_REQUEST['debug'] ) ) {
 	}
 }
 ?><!DOCTYPE html>
+<!--[if IE 6]>
+<html class="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html class="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
+<!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// If paged && not page 1, add the page number
-	essence_the_page_number();
-	?></title>
+	<meta name="viewport" content="width=device-width" />
+	<title><?php echo essence_title(); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
