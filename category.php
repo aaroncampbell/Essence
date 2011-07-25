11109b4abd8dd183745ab4d9076e3e2c447147fc
aaron@xavisys.com <aaron@xavisys.com@cdb4dc37-54d1-4dfa-a9af-8c49da855a76>
2011-07-25 20:56:51 +0000

    Fix the category archive

--- category.php
+++ category.php
@@ -25,8 +25,8 @@ get_header();
 				?></h1>
 				<?php
 					$category_description = category_description();
-					if ( ! empty( $category_description ) ) {
-						echo '<div class="archive-meta">' . $category_description . '</div>';
+					if ( ! empty( $category_description ) )
+						echo "<div class='archive-meta'>{$category_description}</div>";
 
 				/* Run the loop for the category page to output the posts.
 				 * If you want to overload this in a child theme then include a file
