8aa36461b3ddd7b4837daa0d6e5e6b074e5adf2c
Aaron D. Campbell <aaron@xavisys.com>
2012-01-31 12:58:23 -0700

    Work with AND without flex width until flex width exists

--- header.php
+++ header.php
@@ -75,7 +75,10 @@ essence_show_template_file( __FILE__ );
 				// Check to see if the header image has been removed
 				$header_image = get_header_image();
 				if ( ! empty( $header_image ) ) {
-					$header_width = get_header_image_width();
+					if ( function_exists( 'get_header_image_width' ) )
+						$header_width = get_header_image_width();
+					else
+						$header_width = HEADER_IMAGE_WIDTH;
 					$header_height = get_header_image_height();
 			?>
 			<div class="header-image" style="height:<?php echo $header_height; ?>px;">
@@ -91,7 +94,7 @@ essence_show_template_file( __FILE__ );
 							// Houston, we have a new header image!
 							echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
 						} else {
-					?><img src="<?php header_image(); ?>" width="<?php echo $header_width; ?>" height="<?php echo $header_height; ?>" alt="" />
+					?><img src="<?php echo esc_url( $header_image ); ?>" width="<?php echo $header_width; ?>" height="<?php echo $header_height; ?>" alt="" />
 <?php
 						} // end check for featured image or standard header
 					?>
